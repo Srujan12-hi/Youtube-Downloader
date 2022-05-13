e93f4996c2539a552a8b1401ce8817cd9a9be716
Srujan12-hi <85944675+Srujan12-hi@users.noreply.github.com>
2022-05-13 07:44:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("My Dev", url="https://t.me/Sruja_12")],
         [InlineKeyboardButton(
-            "If any bugs", url="https://t.me/Sruja_12_pa_bot")]
+            "If any bugs", url="https://t.me/Sruja_12")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n I am a **Youtube Video downloader bot**\n\n Made By [Srujan](https://t.me/Sruja_12)\n\n I can Download Videos from **Youtube** Links\n Click /help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
